acce122b6d61ff56ae60f0d60aa0fb75c1fe23fb
Leonardo Avendaño <leonardo.a@oppizi.com>
2023-06-24 17:47:13 -0300

    Fix punto5

--- src/uso/Punto5.java
+++ src/uso/Punto5.java
@@ -1,39 +1,59 @@
 package uso;
 import tda.ConjuntoTDA;
 import tda.ColaTDA;
+import tda.PilaTDA;
 import imple.Conjunto;
 
 public class Punto5 {
 	/**
 	 * Devuelve un ConjuntoTDA con los elementos comunes de la pila y de la cola recibidas por argumento.
-	 * Recorre la Cola y por cada valor revisa si existe en el Conjunto parametro. Si existe, lo almacena
-	 * en el Conjunto resultante. Caso contrario, lo ignora.
+	 * Arma dos conjuntos, uno correspondiente a la pila y el otro a la cola. Cada conjunto tiene los valores
+	 * que corresponde a la pila o la cola. Luego se van sacando valores del Conjunto de valores de la Cola
+	 * y revisando si existe en el Conjunto de valores de la Pila. Si existe, se agrega al Conjunto resultante.
+	 * Si no existe, se ignora el valor.
 	 *
 	 * @param cola Cola con valores que se quiere saber si tiene valores en comun contra un conjunto.
-	 * @param conjunto Conjunto contra el cual se va a comparar la Cola.
+	 * @param pila Pila contra el cual se va a comparar la Cola.
 	 * @return Conjunto con los valores comunes entre los dos argumentos.
 	 *
-	 * @precondiciones La Cola y el Conjunto estan inicializados.
+	 * @precondiciones La Cola y la Pila estan inicializados.
 	 *
 	 * @postcondiciones Ninguno.
 	 *
-	 * @costo Polinomica. Revisar si un conjunto tiene un valor tiene costo lineal. Recorrer la Cola
-	 * tiene costo lineal. Como estamos revisando si existe el valor del tope de la Cola en el Conjunto
-	 * el costo es lineal x lineal.
+	 * @costo Lineal. Revisar si un conjunto tiene un valor tiene costo lineal. Convertir la Cola y la Pila
+	 * a Conjunto tiene costo lineal. Como se esta haciendo tres operaciones lineales de forma secuencial, 
+	 * esto tiene un costo lineal.
 	 */
-	static public ConjuntoTDA comunesColaConjunto(ColaTDA cola, ConjuntoTDA conjunto) {
+	static public ConjuntoTDA comunesColaConjunto(ColaTDA cola, PilaTDA pila) {
 		ColaTDA colaClon = ColaHelper.clonar(cola);
-		ConjuntoTDA conjuntoClon = ConjuntoHelper.clonar(conjunto);
+		ConjuntoTDA conjuntoCola = new Conjunto();
+		conjuntoCola.inicializarConjunto();
+		
+		while (!colaClon.colaVacia()) {
+			int valor = colaClon.primero();
+			colaClon.desacolar();
+			conjuntoCola.agregar(valor);
+		}
+		
+		PilaTDA pilaClon = PilaHelper.clonar(pila);
+		ConjuntoTDA conjuntoPila = new Conjunto();
+		conjuntoPila.inicializarConjunto();
+		
+		while (!pilaClon.pilaVacia()) {
+			int valor = pilaClon.tope();
+			pilaClon.desapilar();
+			conjuntoPila.agregar(valor);
+		}
 		
 		ConjuntoTDA comunes = new Conjunto();
 		comunes.inicializarConjunto();
 		
-		while(!colaClon.colaVacia() && !conjuntoClon.conjuntoVacio()) {
-			int valor = colaClon.primero();
-			colaClon.desacolar();
-			if (conjuntoClon.pertenece(valor)) {
+		while(!conjuntoCola.conjuntoVacio() && !conjuntoCola.conjuntoVacio()) {
+			int valor = conjuntoCola.elegir();
+			conjuntoCola.sacar(valor);
+			if (conjuntoPila.pertenece(valor)) {
 				comunes.agregar(valor);
-				conjuntoClon.sacar(valor);
+				conjuntoPila.sacar(valor);
 			} 
 		}
 		
